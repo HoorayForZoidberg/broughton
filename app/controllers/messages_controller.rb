9437573dbe36edcf7c6113adc2b1c2ccef53df86
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-05-29 00:20:59 -0400

    links to contact and home

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
 
     if @message.valid? && verify_recaptcha(model: @message)
       MessageMailer.contact(@message).deliver_now
-      redirect_to new_message_url, notice: "Message received, thanks!"
+      redirect_to root_path, notice: "Message received, thanks!"
     else
       render :new
     end
