aae1e24f4f11d71ea9165c40bac3341b5419d7e2
Nicolas Lizop <nicolas.lizop@gmail.com>
2018-05-30 16:27:18 -0400

    adding comments to issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+    @issue = Issue.find(params[:issue_id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @issue = Issue.find(params[:issue_id])
+    @comment.issue = @issue
+    if @comment.save
+      redirect_to residents_path, notice: "Your comment has been added"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :issue
   belongs_to :user
 
-  validate :content, presence: true
+  validates :content, presence: true
 end
